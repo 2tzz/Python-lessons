a27f6e07f8df7092bdb7e9fe14b49110a968b9ac
2tzz <157218559+2tzz@users.noreply.github.com>
2025-02-05 11:32:32 +0530

    pp

--- lesspn18(BitWiseOp).py
+++ lesspn18(BitWiseOp).py
@@ -1,1 +1,17 @@
 print(~12)
+
+
+#bitwise Or and ANd
+
+
+print(12 & 13)  #and
+
+print(12 | 13)  #or
+
+
+print(12 ^ 13)  #xor
+
+
+print(10 << 2)  #leftshift  shifting 2 bits two left b
+
+print(10>>2)   #right shift
