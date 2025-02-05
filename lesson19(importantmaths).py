0fd8f49b8c70a122bf14a7bcf7a6922fd16291e4
2tzz <157218559+2tzz@users.noreply.github.com>
2025-02-05 14:42:44 +0530

    jj

---
+++ lesson19(importantmaths).py
@@ -0,0 +1,27 @@
+import math
+
+x = math.sqrt(25)
+
+print(x)
+
+
+
+y = math.sqrt (15)
+
+print(y)
+
+print(math.floor(29.9))
+print(math.ceil(25.1))
+
+
+print(math.pow(3,2))
+
+
+print(math.pi)
+print(math.e)
+
+
+z = math.factorial(3)
+
+print(z)
+
